a9c75118127987265f38ba4e76533d404d657ce6
sharofiddinovakbarjon <akbarsharofiddinov@gmail.com>
2025-04-22 10:54:50 +0500

    .

--- src/components/SectionHeader/SectionHeader.tsx
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -3,6 +3,7 @@ import { FiPhone } from "react-icons/fi";
 import { GrLocation } from "react-icons/gr";
 import { useAppDispatch } from "../../store/hooks";
 import { setFeedModal } from "../../store/storeSlice/storeSlice";
+import { FaInstagram, FaTelegram } from "react-icons/fa";
 
 const SectionHeader: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -14,14 +15,29 @@ const SectionHeader: React.FC = () => {
           <div>
             <div className="infos-top">
               <span>
-                <FiPhone color="#ffb300" fontSize={22} />
-                <a href="tel:+998900000000">+998 90 000 00 00</a>
+
+                <a href="tel:+998942679009">
+                  <FiPhone color="#ffb300" fontSize={22} />
+                  +998 94 267 90 09
+                </a>
+              </span>
+              <span>
+                <a href="https://t.me/edison_nukusbot">
+                  <FaTelegram color="#ffb300" fontSize={22} />
+                  @edison_nukusbot
+                </a>
               </span>
             </div>
-            <div className="infos-btm">
+            <div className="infos-top">
               <span>
                 <GrLocation color="#ffb300" fontSize={22} />
                 г.Нукус
+              </span>
+              <span>
+                <a href="https://www.instagram.com/edison_restaurant/">
+                  <FaInstagram color="#ffb300" fontSize={22} />
+                  Instagram
+                </a>
               </span>
             </div>
           </div>
